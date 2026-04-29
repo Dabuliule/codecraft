c85604a510e1ed2064bf1f8817926449f0e8830f
weipengtao <wei_pengtao@qq.com>
2026-04-29 20:00:32 +0800

    feat: 新增 MemoryStore 和 InMemoryStore 模块，实现内存存储接口及其简单实现

--- schema/__init__.py
+++ schema/__init__.py
@@ -1,6 +1,6 @@
 from .action import Action
 from .state import AgentState
 from .step import Step
+from .memory import MemoryItem
 
-__all__ = ["Action", "AgentState", "Step"]
-
+__all__ = ["Action", "AgentState", "Step", "MemoryItem"]
